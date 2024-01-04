c1ead8a5d55428002a99eae7ceb50cc44e4d0894
99omniaashraf <omniaashraf249@gmail.com>
2024-01-03 22:03:48 -0800

    Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches a given patteern.
-puts ARGV[0].scan(/htb?n/).join
+g A regular expression that is matches a given patteern.
+puts ARGV[0].scan(/hb?tn/).join
