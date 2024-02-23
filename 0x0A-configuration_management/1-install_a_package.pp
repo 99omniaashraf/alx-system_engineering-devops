149d368bda80adc156a850a6ab0b1b66800cb2a5
99omniaashraf <omniaashraf249@gmail.com>
2024-02-23 09:25:20 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
+# Install an especific version of flask (2.1.0), werkzeug (2.1.1)
 package {'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package {'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
